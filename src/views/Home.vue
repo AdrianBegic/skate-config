e38ed0275e6c22c055f655a799facca28008394f
AdrianBegic <adrianbegic1@gmail.com>
2022-03-19 20:00:18 -0400

    major

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,51 @@
 <template>
+<Nav :navLinks="navLinks" :navConfig="navConfig"/>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="filler">
+</div>
+    <Carousel />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import Nav from '../components/navBar.vue'
+import Carousel from "@/components/Carousel.vue";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld,
+    Carousel,
+    Nav,
   },
+
+
+setup() {
+        
+
+        const navConfig = ({
+            
+            navBg: "#000",
+            linkFont: "Sans-serif",
+            linkColor: "#fff",
+            linkColorHover: "red",
+            navLogo: "https://interactive-examples.mdn.mozilla.net/media/cc0-images/grapefruit-slice-332-332.jpg",
+        });
+
+        return {
+           navConfig
+        };
+    },
+
+
+
 };
 </script>
+
+<style scoped>
+  .filler {
+  background-image: url("../assets/back.jpg");
+  background-size: cover;
+  height: 100vh;
+  width: 100vw;
+}
+</style>
